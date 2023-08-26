987d20aae2f0d67b207e200125684655cbc524e3
slayonce <katharinemg@gmail.com>
2023-08-26 02:47:17 -0400

    added code for deleteUser in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
   // NOTES: Get all users
@@ -26,8 +26,25 @@ module.exports = {
   async createUser(req, res) {
     try {
       const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
     } catch (err) {
       res.status(500).json(err);
     }
-  }
+  },
+  // NOTES: DELETE USER
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: `No user with the ID: ${req.params.userId}` });
+      }
+      await Thought.deleteMany({ username: user.username });
+
+      res.json({ message: 'User and associated thoughts successfully deleted!' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
