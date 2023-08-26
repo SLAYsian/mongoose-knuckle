332b3338374f7234ef9f1a64c59fe041e852ba47
slayonce <katharinemg@gmail.com>
2023-08-26 13:51:13 -0400

    added friend functionality in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-  // NOTES: Get all users
+  // SECTION: GET ALL USERS
   async getUsers(req, res) {
     try {
       const users = await User.find();
@@ -10,7 +10,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // NOTES: Get One User
+  // SECTION: GET ONE USER
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId }).select('-__v');
@@ -22,7 +22,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // NOTES: CREATE NEW USER
+  // SECTION: CREATE NEW USER
   async createUser(req, res) {
     try {
       const dbUserData = await User.create(req.body);
@@ -31,7 +31,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // NOTES: DELETE USER
+  // SECTION: DELETE USER
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndRemove({ _id: req.params.userId });
@@ -44,6 +44,48 @@ module.exports = {
       res.json({ message: 'User and associated thoughts successfully deleted!' });
     } catch (err) {
       console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // SECTION: ADD FRIEND
+  async addFriend (req, res) {
+    console.log('You are adding a friend');
+    console.log(req.body);
+    try {
+      const user = await User.findOne( { _id: req.params.userId} );
+      if (!user) {
+        return res.status(404).json({ message: `No user with the ID: ${req.params.userId}` });
+      }
+      const friend = await User.findOneAndUpdate(
+        { _id: req.params.friendId }, 
+        { $addToSet: { friends: user._id} }, 
+        { runValidators: true, new: true } 
+        );
+        if (!friend) {
+          return res.status(404).json({ message: `No user with the ID: ${req.params.friendId} to add as friend!` });
+        }
+        res.json(friend);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // SECTION: DELETE FRIEND
+  async removeFriend (req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId});
+      if (!user) {
+        return res.status(404).json({ message: `No user with the ID: ${req.params.userId}` });
+      }
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.userId }, 
+        { $pull: { friends: req.params.friendId } }, 
+        { runValidators: true, new: true } 
+        );
+        if (!updatedUser) {
+          return res.status(404).json({ message: `No user with the ID: ${req.params.userId}!` });
+        }
+        res.json(updatedUser);
+    } catch (err) {
       res.status(500).json(err);
     }
   },
