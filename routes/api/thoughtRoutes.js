965f9bb27ff368cb52ce718398c1910b65deb4ac
slayonce <katharinemg@gmail.com>
2023-08-26 16:15:05 -0400

    added functionality for thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { getAllThoughts, getSingleThought, createThought, updateThought, deleteThought, addThoughtReaction, removeThoughtReaction } = require('../../controllers/thoughtController');
+
+// SECTION: /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// SECTION: /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// SECTION: /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addThoughtReaction)
+
+// SECTION: /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
